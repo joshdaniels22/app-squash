cab4dbf81fe786fd770c9413599566dd364b205f
Joshua Daniels <joshuadaniels@Joshuas-MacBook-Air.local>
2024-06-01 00:20:13 +0200

    login page and colour scheme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# app-squash
-Squash court booking website for my club.
+# OM Squash App
+An app for court booking at my squash club.
+
+## Colour Palette
+#(white)
+#9de0ad
+#584f4f
+
+Inspiration: `https://www.color-hex.com/color-palette/15805`
+
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,60 @@
+<svelte:head>
+	<title>Login</title>
+	<meta name="description" content="Login Page" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>OM Squash</h1>
+
+  <input type="text" placeholder="Username">  
+  <input type="password" placeholder="Password">
+  <button>Login</button>
+
+</div>
+
+<style>
+  div {
+    width: 50%;
+    background: #001a33;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justtify-content: space-between;
+  }
+
+  h1 {
+    color: white;
+  }
+
+	input {
+    height: 30px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+		width: 80%;
+    border: none; 
+    border-radius: 6px;
+    box-shadow: 5px 5px #9de0ad;
+    text-align: center;
+	}
+
+  button {
+    height: 30px;
+    background: #9de0ad;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    width: 80%;
+    border: none;
+    border-radius: 6px;
+    box-shadow: 5px 5px #547980;
+  }
+
+button:hover {
+/*	text-decoration: underline; */
+  color: white;
+    box-shadow: 9px 9px #547980;
+}
+
+  ::placeholder {
+      text-align: center;
+    }
+</style>
